dd84847fc7ff6ffa21c01c517de24b4191a712a5
Petr Krejčík <petr.krejcik@socialbakers.com>
2019-03-19 09:36:57 +0100

    ro.8

--- git/setup.sh
+++ git/setup.sh
@@ -6,22 +6,25 @@ read -p "- name the file 'github' (copy entire filepath)"
 read -p "- paste (!using right click + Paste) password from MacPass -> GitHub SSH"
 read -p "- When prompted now for passphrase try hitting Enter without password (to verify empty password)"
 ssh-keygen -o -t rsa -b 4096 -C "github.com"
+sleep 3
 ssh-add -K $HOME/.ssh/github
 echo "Copy SSH public key to github.com"
+read -p "- login to github.com"
 pbcopy < $HOME/.ssh/github.pub
-read -p "- open github.com -> Settings -> SSH and GPG keys -> New SSH key or Add SSH key"
+read -p "- github.com -> Settings -> SSH and GPG keys -> New SSH key or Add SSH key"
 read -p "- name: 'Mac 1', key: cmd + V"
 
 
 cd $HOME/www/config
 git remote set-url origin git@github.com:petrkrejcik/config.git
-git push # to trigger login
+git fetch # to trigger login
 
 echo "Generating SSH keys for GitLab"
 read -p "- name the file 'gitlab' (copy entire filepath)"
 read -p "- paste (!using right click + Paste) password from MacPass -> SBKS - GitLab SSH"
 read -p "- When prompted now for passphrase try hitting Enter without password (to verify empty password)"
 ssh-keygen -o -t rsa -b 4096 -C "gitlab.com"
+sleep 3
 ssh-add -K $HOME/.ssh/gitlab
 echo "Copy SSH public key to gitlab.ccl"
 pbcopy < $HOME/.ssh/gitlab.pub
