9922574785abb3bda043c870dcad97d571364889
Petr Krejčík <petr.krejcik@socialbakers.com>
2019-03-21 10:46:10 +0100

    debug

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -7,7 +7,7 @@ mkdir $HOME/.nvm
 echo 'export NVM_DIR="$HOME/.nvm"' >> $HOME/.bash_profile
 echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> $HOME/.bash_profile
 echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> $HOME/.bash_profile
-NVM_DIR="$HOME/.nvm"
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
