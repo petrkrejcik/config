93c0596869fa2512488d6b27ea76649074274cff
Petr Krejčík <petr.krejcik@socialbakers.com>
2019-03-21 10:57:49 +0100

    d

--- node/install.sh
+++ node/install.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install node # install latest
--- nvm/install.sh
+++ nvm/install.sh
@@ -2,3 +2,6 @@
 
 # Requires NVM_DIR in .bash_profile
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
