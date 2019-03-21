fccaa30cf44030a7cf08041a22c93c6d1b4d66ee
Petr Krejčík <petr.krejcik@socialbakers.com>
2019-03-21 10:22:31 +0100

    r0.17

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -3,6 +3,7 @@
 # bash_profile
 touch $HOME/.bash_profile
 
+mkdir $HOME/.nvm
 echo 'export NVM_DIR="$HOME/.nvm"' >> $HOME/.bash_profile
 echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> $HOME/.bash_profile
 echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> $HOME/.bash_profile
