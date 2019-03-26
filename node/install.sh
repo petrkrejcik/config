e852726d07ef9bdef5cafec10fb280c3cbf0b999
Petr Krejčík <petr.krejcik@socialbakers.com>
2019-03-26 15:05:43 +0100

    r1.4

--- node/install.sh
+++ node/install.sh
@@ -5,4 +5,5 @@ NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-nvm install node # install latest
+# nvm install node # install latest
+nvm install 10 # install latest
